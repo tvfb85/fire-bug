09c4561af8ec50fcc162ee084fcaeebc148c4a08
Vicky Roberts <vicki_uk@hotmail.com>
2017-03-02 09:40:31 +0000

    second user story test passing

--- spec/features/player_names_spec.rb
+++ spec/features/player_names_spec.rb
@@ -11,3 +11,17 @@ RSpec.feature "Enter players" do
     expect(page).to have_text("Jack vs Daniel")
   end
 end
+
+# As Player 1,
+# So I can see how close I am to winning
+# I want to see Player 2's Hit Points
+
+RSpec.feature "See Hit Points" do
+  scenario "view opponent's hit points" do
+    visit '/'
+    fill_in :player_one_name, :with => "Jack"
+    fill_in :player_two_name, :with => "Daniel"
+    click_button "Submit players"
+    expect(page).to have_content("Daniel: 60 HP")
+  end
+end

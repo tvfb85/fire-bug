3b7476dabd48feec893ccb576034db6d96479c1c
Vicky Roberts <vicki_uk@hotmail.com>
2017-03-02 09:37:08 +0000

    POST-redirect loop added, enabled session to store params and refactored

--- app.rb
+++ app.rb
@@ -1,15 +1,24 @@
 require 'sinatra/base'
 
 class Firebug < Sinatra::Base
+  enable :sessions
+
   get '/' do
     erb(:index)
   end
 
   post '/player_names' do
-    @player_one_name = params[:player_one_name]
-    @player_two_name = params[:player_two_name]
+    session[:player_one_name]  = params[:player_one_name]
+    session[:player_two_name]  = params[:player_two_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_one_name = session[:player_one_name]
+    @player_two_name = session[:player_two_name]
     erb(:play)
   end
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0
